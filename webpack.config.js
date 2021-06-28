24e1c82868256f02b2b7f31d02dbcb23503616b3
vvladlena <77752981+vvladlena@users.noreply.github.com>
2021-06-28 20:43:10 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = "/";
+  const publicPath = "vue_project/";
 
   const pcss = {
     test: /\.(p|post|)css$/,
