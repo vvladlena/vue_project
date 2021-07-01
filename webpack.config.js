451a2fbe11794d76e462cdeabb5df4223b837692
vvladlena <77752981+vvladlena@users.noreply.github.com>
2021-07-01 17:23:22 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = "vue_project/";
+  const publicPath = "../vue_project/";
   // const publicPath = "/";
 
   const pcss = {
